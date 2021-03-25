ec7a30f03c1ef4b4783494b048a7f1343dd79cbe
Connor Ngo <LoneAuios@gmail.com>
2021-03-25 14:40:59 -0400

    Initial

---
+++ Auios.WebServer/Auios.WebServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using Sys = System;
+
+namespace Auios.WebServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sys.Console.WriteLine("Hello World!");
+        }
+    }
+}
